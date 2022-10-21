3da4909088d57e78b8f8b2a83375aede4cddf8e3
Andrii Martyniuk <94974356+Surstr0mming@users.noreply.github.com>
2022-10-21 14:15:53 +0300

    Martyniuk Andrii CW№6, lab4

---
+++ test_package/CW1_2.java
@@ -0,0 +1,10 @@
+package test_package;
+public class CW1_2 {
+    public static void main(String[] args) {
+        Test_Class  test = new  Test_Class();
+        test.setT(2);
+        test.setD(0.818933027098955175);
+        test.str = "one more time";
+        System.out.println(test.t +" \n"+ test.d + "\n" + test.str );
+    }
+}
